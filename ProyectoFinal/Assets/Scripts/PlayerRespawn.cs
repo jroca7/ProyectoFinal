09bf0c8ac488925e93f55f8fef485a9cd7aa675c
jroca7 <70997708+jroca7@users.noreply.github.com>
2021-08-08 05:17:12 -0300

    2 Commit

---
+++ ProyectoFinal/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerRespawn : MonoBehaviour
+{
+
+    public Animator animator;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    public void PlayerDamaged()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+}
